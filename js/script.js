75ede771e0c82d48cdfc93dc32419bcc1a625d87
{tatsuya-m-nisato@outlook.jp} <tatsuya-m-nisato@outlook.jp>
2022-03-08 00:31:05 +0900

    still changing

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-var headerHeight = $("header").outerHeight(true);
+var headerHeight = $("#header").outerHeight(true);
 
 function FixedAnime(){
     var scroll =$(window).scrollTop();
     if(scroll>=headerHeight){
-        $("header").addClass(".shrinked-header");
+        $("#header").addClass("shrinked-header");
     }else{
-        $("header").removeClass(".shrinked-header");
+        $("#header").removeClass("shrinked-header");
     }
 }
+
