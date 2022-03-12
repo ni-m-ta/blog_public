908196da4e663aeee50b1aca291df1c8208df597
{tatsuya-m-nisato@outlook.jp} <tatsuya-m-nisato@outlook.jp>
2022-03-12 10:00:07 +0900

    create the shrinkable header

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-var headerHeight = $("#header").outerHeight(true);
+$(function(){
+    var $window = $(window),
+    $headerOptions = $('.header-logo-snsIcons');
+    $header = $('header');
 
-function FixedAnime(){
-    var scroll =$(window).scrollTop();
-    if(scroll>=headerHeight){
-        $('#header').addClass('.shrinked-header');
-    }else{
-        $('#header').removeClass('.shrinked-header');
-    }
-}
+    $window.on('load scroll', function(){
+        var value = $(this).scrollTop();
+        if(value > 130){
+            $headerOptions.addClass('removed-options');
+            $header.addClass('shrinked-header');
+        }else{
+            $headerOptions.removeClass('removed-options');
+            $header.removeClass('shrinked-header');
+        }
+    });
+});
  
