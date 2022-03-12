447fa90fea11c6c0f37f4c1c7fd0ae42a9ebf31b
{tatsuya-m-nisato@outlook.jp} <tatsuya-m-nisato@outlook.jp>
2022-03-12 09:05:59 +0900

    creating the header to shrink

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ var headerHeight = $("#header").outerHeight(true);
 function FixedAnime(){
     var scroll =$(window).scrollTop();
     if(scroll>=headerHeight){
-        $("#header").addClass("shrinked-header");
+        $('#header').addClass('.shrinked-header');
     }else{
-        $("#header").removeClass("shrinked-header");
+        $('#header').removeClass('.shrinked-header');
     }
 }
-
+ 
