7c7cbbcc16ed3e6d4a68f3345ef0063fe568fc47
{tatsuya-m-nisato@outlook.jp} <tatsuya-m-nisato@outlook.jp>
2022-03-14 01:48:51 +0900

    create the dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 $(function(){
     var $window = $(window),
-    $headerOptions = $('.header-logo-snsIcons');
+    $headerSNSIcons = $('.header-logo-snsIcons');
     $header = $('header');
+    $headerOptions = $('.header-option-parent');
 
     $window.on('load scroll', function(){
         var value = $(this).scrollTop();
         if(value > 130){
-            $headerOptions.addClass('removed-options');
+            $headerSNSIcons.addClass('removed-options');
             $header.addClass('shrinked-header');
         }else{
-            $headerOptions.removeClass('removed-options');
+            $headerSNSIcons.removeClass('removed-options');
             $header.removeClass('shrinked-header');
         }
     });
+
+    $headerOptions.hover(function(){
+            $(".header-dropdown-menu:not(:animated)", this).slideDown();
+        }, function(){
+            $(".header-dropdown-menu", this).slideUp();
+        });
+
 });
  
