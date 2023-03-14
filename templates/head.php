172feee95668a83e62e94c533e9a1d619fec54bc
ni-m-ta <tatsuyanisato@gmail.com>
2023-03-13 17:48:15 -0700

    add google adsense

--- templates/head.php
+++ templates/head.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
- <html lang="ja">
+<html lang="ja">
   <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-TSPKVQ1899"></script>
@@ -10,13 +10,12 @@
 
   gtag('config', 'G-TSPKVQ1899');
 </script>
-   <meta charset="UTF-8">
-   <title><?php echo $title; ?></title>
-   <meta name="description" content="<?php echo $description; ?>">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="icon" href="../img/iconForTab.png">
-   <link rel="stylesheet" href="../css/main.css">
-   <link rel="stylesheet" href="../css/contents.css">
-   <script src="https://kit.fontawesome.com/00f67a9916.js" crossorigin="anonymous"></script>
-   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3886315435685807"
-     crossorigin="anonymous"></script>
+  <meta charset="UTF-8">
+  <title><?php echo $title; ?></title>
+  <meta name="description" content="<?php echo $description; ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="../img/iconForTab.png">
+  <link rel="stylesheet" href="../css/main.css">
+  <link rel="stylesheet" href="../css/contents.css">
+  <script src="https://kit.fontawesome.com/00f67a9916.js" crossorigin="anonymous"></script>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3886315435685807"crossorigin="anonymous"></script>
